5fd253fee002d0eef0a5e22df2396a557610674d
RAMAZAN GENCER <73017548+rmzngencer@users.noreply.github.com>
2022-12-23 16:47:26 +0300

    Update detail.php

--- okaywebpart/detail.php
+++ okaywebpart/detail.php
@@ -1,5 +1,8 @@
-<?php include("connect.php");
- ?>
+
+<?php
+include("connect.php");
+include("userinfo.php");
+?>
 
 <?php 
 
@@ -426,7 +429,32 @@ button.addEventListener('click', function() {
     });
   });
 </script>
-
+<script>
+    function openPage(pageName,elmnt,color) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablink");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+      }
+      document.getElementById(pageName).style.display = "block";
+      elmnt.style.backgroundColor = color;
+    }
+    
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+    </script>
+    <script>
+         
+         let subMenu = document.getElementById("subMenu");
+     
+         function toggleMenu() {
+             subMenu.classList.toggle("open-menu");
+         }
+     </script>
 </body>
 </html>
 
