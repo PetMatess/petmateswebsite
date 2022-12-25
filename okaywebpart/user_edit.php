6566e0f248fc9593e0a4863d0bc38110e7a65be2
RAMAZAN GENCER <73017548+rmzngencer@users.noreply.github.com>
2022-12-25 17:49:45 +0300

    profil resmi bağlandı

--- okaywebpart/user_edit.php
+++ okaywebpart/user_edit.php
@@ -118,7 +118,7 @@ include("userinfo.php");
           <div class="col-md-5 border-right">
             <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5"
                 width="150px"
-                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span
+                src=<?php echo "uploads/".$user_img;?>><span
                 class="font-weight-bold"><?php echo "$name" ?> <?php echo "$lastname" ?></span><span
                 class="text-black-50"><?php echo "$email" ?></span><span>
               </span></div>
